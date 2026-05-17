724fa7152632f58b4c7c4bf577270215121d9a97
madhusailesh <madhusaileshsasamal998@gmail.com>
2026-05-17 15:16:14 +0530

    feat: implement authentication routes with signup and login functionality

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
 const app = express();
 
 app.use(cors());
-
+dotenv.config();
 const server = http.createServer(app);
 
 const io = new Server(server, {
