b992f3d1d66261fa3f4d37aeab5df37f6f0dfa14
madhusailesh <madhusaileshsasamal998@gmail.com>
2026-05-17 12:59:49 +0530

    str init

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User Connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected");
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
